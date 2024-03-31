f81e688e6fafa992739f3e77795cb84c0ed987b1
daveS <david.surina22@gmail.com>
2024-03-31 14:04:12 +0200

    added dummy products and rendering

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,35 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+export const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "First book",
+    description: "First special book",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "Second book",
+    description: "Second special book",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            id={product.id}
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
