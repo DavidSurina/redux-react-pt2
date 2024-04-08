4f714c3811d1eb1ca6062cfcb94a8bf7cad8e7a3
daveS <david.surina22@gmail.com>
2024-04-08 19:50:00 +0200

    cart data status constant

---
+++ src/constants/cartData.js
@@ -0,0 +1,17 @@
+export const CART_DATA_STATUS = {
+  SUCCESS: {
+    status: "success",
+    title: "Success!",
+    message: "Sent cart data successfully!",
+  },
+  PENDING: {
+    status: "pending",
+    title: "Sending",
+    message: "Sending cart data!",
+  },
+  ERROR: {
+    status: "error",
+    title: "Error!",
+    message: "Sending cart data failed!",
+  },
+};
